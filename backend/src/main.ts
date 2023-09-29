f14ebeeb80657fd867fa35df3b693689b7814488
Natã <natasjesus1@gmail.com>
2023-09-29 16:26:07 -0300

    chore(api): use cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
 
   const config = new DocumentBuilder()
     .setTitle('Documentação da api quaestum Desafio')
