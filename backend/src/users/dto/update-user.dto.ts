350c29f44d3ca2eae79f0be1786f5985f98932a7
Natã <natasjesus1@gmail.com>
2023-10-01 21:07:16 -0300

    refactor(user): Remove PartialType, use class-validator to prevent accidental password changes
    In this commit, the PartialType has been removed from the UpdateUserDto and class-validator is now used to prevent unintentional changes to the password field. The password field is explicitly excluded from updates.

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,22 +1,24 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNumber, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
   /**
    * Nome do usuário que será atualizado
    * @example 'Sérgio Freitas'
    */
+  @IsString()
   name?: string;
 
   /**
    * Idade do usuário que será atualizado
    * @example 40
    */
+  @IsNumber()
   age?: number;
 
   /**
    * Email do usuário que será atualizado
    * @example 'sergio@example.com'
    */
+  @IsEmail()
   email?: string;
 }
